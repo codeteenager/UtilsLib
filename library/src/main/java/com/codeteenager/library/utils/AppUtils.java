1c2875263b6ae321076adf9d58c2f9094fa5fb54
unknown <>
2017-05-11 14:15:41 +0800

    Initial commit

---
+++ library/src/main/java/com/codeteenager/library/utils/AppUtils.java
@@ -0,0 +1,47 @@
+package com.codeteenager.library.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by codeteenager on 2017/5/10.
+ * 获取应用程序相关信息
+ */
+
+public class AppUtils {
+    /**
+     * 获取应用程序名称
+     */
+    public static String getAppName(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(
+                    context.getPackageName(), 0);
+            int labelRes = packageInfo.applicationInfo.labelRes;
+            return context.getResources().getString(labelRes);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * [获取应用程序版本名称信息]
+     *
+     * @param context
+     * @return 当前应用的版本名称
+     */
+    public static String getVersionName(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(
+                    context.getPackageName(), 0);
+            return packageInfo.versionName;
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ library/src/main/java/com/codeteenager/library/utils/LogUtils.java
@@ -0,0 +1,32 @@
+package com.codeteenager.library.utils;
+
+import android.util.Log;
+
+/**
+ * Created by codeteenager on 2017/5/10.
+ * 日志打印相关类
+ */
+
+public class LogUtils {
+    public static boolean isDebug = true;// 是否需要打印bug，可以在application的onCreate函数里面初始化
+
+    public static void i(String tag, String msg) {
+        if (isDebug)
+            Log.i(tag, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (isDebug)
+            Log.d(tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (isDebug)
+            Log.e(tag, msg);
+    }
+
+    public static void v(String tag, String msg) {
+        if (isDebug)
+            Log.v(tag, msg);
+    }
+}
